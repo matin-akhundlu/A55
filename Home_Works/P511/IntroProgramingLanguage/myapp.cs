0224aa812ac652faee245da18c2a77488901b775
Matin Akhundlu <75532364+matin-a-g@users.noreply.github.com>
2020-12-12 23:28:23 -0500

    update

--- Home_Works/P511/IntroProgramingLanguage/myapp.cs
+++ Home_Works/P511/IntroProgramingLanguage/myapp.cs
@@ -4,7 +4,7 @@ namespace MyFirstApplication {
     class HeyGuys {
         static void Main(string[] args) {
 	/* My First Program in C# */
-        Console.WriteLine("Hello Guys!");
+        Console.WriteLine("Hey Guys!");
 	Console.ReadKey();
         }
     }
