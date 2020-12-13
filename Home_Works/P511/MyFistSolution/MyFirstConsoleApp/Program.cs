93606972eef09f3a4c904188c3a6be2529eb645c
Matin Akhundlu <75532364+matin-a-g@users.noreply.github.com>
2020-12-13 02:26:14 -0500

    update

---
+++ Home_Works/P511/MyFistSolution/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Life is Beautiful");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Just touch your dreams");
+
+            Console.ReadLine();
+
+            Console.WriteLine("BYE");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
