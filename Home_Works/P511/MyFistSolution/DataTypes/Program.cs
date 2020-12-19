034c6b5c26f5d43919fa9c858dd54bc386dc3222
Matin Akhundlu <75532364+matin-a-g@users.noreply.github.com>
2020-12-18 22:26:36 -0500

    update

---
+++ Home_Works/P511/MyFistSolution/DataTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Info");
+
+            byte a = 4;
+
+            int sizeOfByte = sizeof(byte);
+
+            Console.WriteLine($"----BYTE----" +
+                $"\nsize={sizeOfByte} byte" +
+                $"\nvalue={a}" +
+                $"\nMinValue={byte.MinValue}" +
+                $"\nMaxValue={byte.MaxValue}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
