cee5f99cd284e5484c3637766ce223fc6aa11e4c
Matin Akhundlu <75532364+matin-a-g@users.noreply.github.com>
2020-12-18 23:13:57 -0500

    Update

--- Home_Works/P511/MyFistSolution/DataTypes/Program.cs
+++ Home_Works/P511/MyFistSolution/DataTypes/Program.cs
@@ -11,12 +11,34 @@ namespace DataTypes
             byte a = 4;
 
             int sizeOfByte = sizeof(byte);
+            var defualtValueOfByte = default(byte);
+            //tyfeof(byte).Name
+            
 
             Console.WriteLine($"----BYTE----" +
                 $"\nsize={sizeOfByte} byte" +
                 $"\nvalue={a}" +
                 $"\nMinValue={byte.MinValue}" +
-                $"\nMaxValue={byte.MaxValue}");
+                $"\nMaxValue={byte.MaxValue}" +
+                $"\nDefault={defualtValueOfByte}" +
+                $"\nCts Type Name={a.GetType().Name}");
+
+
+
+            int b = 4;
+
+            int sizeOfint = sizeof(int);
+            //var defualtValueOfByte = default(int);
+            ////tyfeof(byte).Name
+
+
+            Console.WriteLine($"----{b.GetType().Name}----" +
+                $"\nsize={sizeOfint} byte" +
+                $"\nvalue={b}" +
+                $"\nMinValue={int.MinValue}" +
+                $"\nMaxValue={int.MaxValue}" +
+                $"\nDefault={default(int)}" +
+                $"\nCts Type Name={b.GetType().Name}");
 
 
             Console.ReadKey();
