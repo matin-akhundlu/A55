6083448b47054add7884c157a20fbbc6d92d7de8
Matin Akhundlu <75532364+matin-a-g@users.noreply.github.com>
2021-01-11 17:09:12 -0500

    update

---
+++ Class/Class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            Class1 M55 = new Class1(1);
+            M55.user = 1;
+            M55.Title = "Aut nihil aut P511.";
+            M55.Completed = true;
+            Console.WriteLine(M55);
+
+
+            Console.ReadKey();
+        }
+    }
+}
